6ffc2fcacf5a1990c135828da65c55a57d321233
Nikolai Prokofev <nvprokofiev@gmail.com>
2021-01-17 22:55:04 -0500

    Initial Commit

---
+++ ChangeWatcher/ChangeWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeWatcherApp.swift
+//  ChangeWatcher
+//
+//  Created by Nikolai Prokofev on 2021-01-17.
+//
+
+import SwiftUI
+
+@main
+struct ChangeWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
