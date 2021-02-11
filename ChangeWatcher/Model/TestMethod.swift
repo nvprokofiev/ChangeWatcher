585ab217aec2b9a068793fd27b4f174e99c17957
Nikolai Prokofev <nvprokofiev@gmail.com>
2021-02-10 21:44:14 -0500

    add test method enum

---
+++ ChangeWatcher/Model/TestMethod.swift
@@ -0,0 +1,14 @@
+//
+//  TestMethod.swift
+//  ChangeWatcher
+//
+//  Created by Nikolai Prokofev on 2021-02-10.
+//
+
+import Foundation
+
+enum TestMethod: Int {
+    case stringContent
+    case headlessBrowser
+    case headlessBrowserWithDelay
+}
